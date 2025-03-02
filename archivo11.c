0abf93c4c10476ae7587d41950297919ad21e03a
Ricardo Ríos Cruz <balenrivers3@gmail.com>
2025-03-01 20:04:39 -0600

    Create archivo11.c

---
+++ archivo11.c
@@ -0,0 +1,22 @@
+// 11. Buscar en un Arreglo
+#include <stdio.h>
+
+int *buscarElemento(int *arr, int tamaño, int valor) {
+    // Implementar búsqueda aquí
+    int *direc;
+    for(int j= 0; j<tamaño; j++){
+            if(arr[j]==valor){
+                direc= &arr[j];
+            }
+        }
+    printf("la posición es: %p", direc);
+    return NULL;
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int tamaño= 5, valor= 30;
+    // Código para buscar un número y mostrar su dirección
+    *buscarElemento(arr, tamaño, valor);
+    return 0;
+}
